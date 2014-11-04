0d3650c590868589a3b16695bd661c190c5506a0
Panagiotis <sl45sms@gmail.com>
2014-11-04 14:09:40 +0200

    Update easter.groovy
    add catholic easter

--- easter.groovy
+++ easter.groovy
@@ -1,5 +1,26 @@
 import java.text.DateFormat
 
+Date catholic_easter(int y)
+    {
+        int a = y % 19;
+        int b = y / 100;
+        int c = y % 100;
+        int d = b / 4;
+        int e = b % 4;
+        int g = (8 * b + 13) / 25;
+        int h = (19 * a + b - d - g + 15) % 30;
+        int j = c / 4;
+        int k = c % 4;
+        int m = (a + 11 * h) / 319;
+        int r = (2 * e + 2 * j - k - h + m + 32) % 7;
+        int easterMonth = (h - m + r + 90) / 25;
+        int easterDay = (h - m + r + easterMonth + 19) % 32;
+  
+     def easter = y+"-"+easterMonth+"-"+easterDay
+     println easter
+     return new Date().parse("yyyy-M-d", easter)
+    }
+    
 Date easter(int y){
 /* ΜΑΘΗΜΑΤΙΚΟΣ ΤΥΠΟΣ ΥΠΟΛΟΓΙΣΜΟΥ ΤΗΣ ΗΜΕΡΟΜΗΝΙΑΣ ΤΟΥ ΟΘΡΟΔΟΞΟΥ ΠΑΣΧΑ
 (Χ = χρόνος που θέλουμε να βρούμε το Πάσχα).
@@ -10,7 +31,6 @@ Date easter(int y){
 {2β+4γ+6δ+6}: 7          το υπόλοιπο από τη διαίρεση = ε
 Πάσχα = δ+ε+4 Απριλίου (Μαΐου αν ο αριθμός>30)
 */
-    
     int a=y%19;
     int b=y%4;
     int c=y%7;
